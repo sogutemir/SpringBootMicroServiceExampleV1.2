03f0e9c910b486dca169aec60c6d35870c43730c
sogutemir <108363656+sogutemir@users.noreply.github.com>
2024-08-16 21:13:59 +0300

    ProjectCreated

---
+++ user-service/src/main/java/org/work/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package org.work.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
