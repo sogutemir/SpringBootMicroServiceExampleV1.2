ce53ed729af5def15203929ca613eae66e833049
sogutemir <108363656+sogutemir@users.noreply.github.com>
2024-08-20 21:01:49 +0300

    Update GatewayConfig.java

--- GlobalApiGateway/src/main/java/org/work/globalapigateway/config/GatewayConfig.java
+++ GlobalApiGateway/src/main/java/org/work/globalapigateway/config/GatewayConfig.java
@@ -6,20 +6,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ApiGatewayConfig {
+public class GatewayConfig {
 
     @Bean
-    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("account-service", r -> r.path("/api/accounts/**")
+                .route("account-service", r -> r.path("/account/**")
                         .uri("http://account-service:8080"))
-                .route("user-service", r -> r.path("/api/users/**")
+                .route("user-service", r -> r.path("/user/**")
                         .uri("http://user-service:8080"))
-                .route("order-service", r -> r.path("/api/orders/**")
+                .route("order-service", r -> r.path("/order/**")
                         .uri("http://order-service:8080"))
-                .route("product-service", r -> r.path("/api/products/**")
+                .route("product-service", r -> r.path("/product/**")
                         .uri("http://product-service:8080"))
-                .route("notification-service", r -> r.path("/api/notifications/**")
+                .route("notification-service", r -> r.path("/notification/**")
                         .uri("http://notification-service:8080"))
                 .build();
     }
